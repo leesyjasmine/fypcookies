f5b8db284d9bb4a3b4894e1a64e882c547bd278e
mkkoh <mkkoh@users.noreply.github.com>
2016-07-28 11:46:23 +0800

    Add files via upload
    1.   Fixed some typos
    2.   Removed respective headings for a cleaner look

--- cookiemanager.js
+++ cookiemanager.js
@@ -474,7 +474,9 @@ $(function () {
 				$("#modifyCookie").append(element);
 			}
 
-			//Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons. The "active" class is used to add a background color to the current button when its belonging panel is open. The "show" class is used to open the specific accordion panel
+			//Toggle between adding and removing the "active" and "show" classes when the user clicks on one of the "Section" buttons.
+			//The "active" class is used to add a background color to the current button when its belonging panel is open.
+			//The "show" class is used to open the specific accordion panel.
 			var acc = document.getElementsByClassName("accordion");
 			var a;
 
@@ -767,7 +769,7 @@ $(function () {
 						toFile(getEncrypted(text), 'downloadLink2', exportFilter);
 					else if (option === "profile")
 						toProfile(text);
-					$("#exportDialog p").text("Cookies from www" + cookieArray[0].domain + " successfully exported to " + option + ".");
+					$("#exportDialog p").text("Cookies from " + cookieArray[0].domain + " successfully exported to " + option + ".");
 				} catch (err) {
 					$("#exportDialog p").text("Export Error");
 				}
